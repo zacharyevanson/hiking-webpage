e243efe8ba83912f6b89764b9d99ea822fb6b0b7
zacharyevanson <evansonzach@gmail.com>
2025-04-09 11:01:19 +0200

    Updated comments for script files to ensure clear documentation.

--- scripts/likes.js
+++ scripts/likes.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
+    // allow user to 'like' hike
     let likeBtn = document.querySelector(".like-btn")
     let hikeName = likeBtn.getAttribute("data-hike")
+    // retrieving number of likes for a hike from session storage
     let likes = JSON.parse(sessionStorage.getItem("hikeLikes")) || {};
 
     if (!likes[hikeName]) {
+        // initializing likes for a hike without any likes
         likes[hikeName] = 0;
+        // setting session storage to carry over likes
         sessionStorage.setItem("hikeLikes", JSON.stringify(likes));
     }
 
     $("#likes-" + hikeName).text(likes[hikeName]);
 
     $(".like-btn").click(function () {
+        // on click, increment number of likes by 1
         likes[hikeName] += 1;
         sessionStorage.setItem("hikeLikes", JSON.stringify(likes));
 
